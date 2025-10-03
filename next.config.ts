e1b6fcbde752401830d95ffc01231c20409843bd
madegwa-o <madegwaoscar317@gmail.com>
2025-10-03 20:58:59 +0300

    iron heart

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+                pathname: '/api/images/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.vercel.app',
+                pathname: '/api/images/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'duka-sandy.vercel.app',
+                pathname: '/api/images/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'pub-*.r2.dev', // R2 public URLs
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'a13bab3063d300082d0a15d610f46cb4.r2.cloudflarestorage.com', // Your R2 endpoint
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
