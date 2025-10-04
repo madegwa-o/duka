9fc351feae3a25aa514c4c926c8ce8ddda3d3548
madegwa-o <madegwaoscar317@gmail.com>
2025-10-04 07:54:48 +0300

    pwa take 3.1

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,18 @@
-import type { NextConfig } from "next";
+
+import withPWAInit from '@ducanh2912/next-pwa';
+import type { NextConfig } from 'next';
+
+
+const withPWA = withPWAInit({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    reloadOnOnline: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
 
 const nextConfig: NextConfig = {
     images: {
@@ -33,4 +47,6 @@ const nextConfig: NextConfig = {
     },
 };
 
-export default nextConfig;
+
+
+export default withPWA(nextConfig);
