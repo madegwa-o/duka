f3e199a58f2f4b32a434c7dd9cff1175681c4a53
madegwa-o <madegwaoscar317@gmail.com>
2025-10-19 19:46:27 +0300

    push notifications

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,5 +7,6 @@ import { Product } from "@/models/Product";
 import { Shop } from "@/models/Shop";
 import { User } from "@/models/User";
 import { Image } from "@/models/Image";
+import { Subscription } from "@/models/Subscription";
 
-export { Category, Product, User, Shop, Image };
+export { Category, Product, User, Subscription, Shop, Image };
